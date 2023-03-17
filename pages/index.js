201b939e814e88589ce7f2d9d253c8985f407a60
Jahnavi Yelamanchi <jahnaviyelamanchi03@gmail.com>
2023-03-17 18:07:12 +0530

    new-file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
 import {useState} from 'react';
 
 const Home = () => {
@@ -37,7 +35,7 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 Writer</title>
       </Head>
       <div className="container">
         <div className="header">
@@ -80,18 +78,6 @@ const Home = () => {
             </div>
           )}
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
       </div>
     </div>
   );
